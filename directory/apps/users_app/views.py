60c336b6740053995fcc7d41fb367df7230663e0
Alireza Ayinmehr <alireza.darksun@gmail.com>
2020-01-30 13:35:15 +0330

    Add new views for auth

--- directory/apps/users_app/views.py
+++ directory/apps/users_app/views.py
@@ -1,6 +1,11 @@
-from flask import request, abort
+from flask import abort, request
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                get_jwt_identity, jwt_refresh_token_required,
+                                jwt_required)
 from sqlalchemy.exc import IntegrityError
+
 from directory import db
+
 from . import users
 from .models import User
 
@@ -26,3 +31,41 @@ def create_user():
         return {'error': 'Username is duplicated.'}, 400
 
     return {'message': 'Account created successfully'}, 201
+
+
+@users.route('/auth/', methods=['POST'])
+def login():
+    if not request.is_json:
+        return {'error': 'JSON Only!'}, 400
+    
+    args = request.get_json()
+
+    username = args.get('username')
+    password = args.get('password')
+
+    user = User.query.filter(User.username.ilike(username)).first()
+    if not user:
+        return {'error': 'Username/Password does not match.'}, 403
+    
+    if not user.check_password(password):
+        return {'error': 'Username/Password does not match.'}, 403
+
+    access_token = create_access_token(identity=user.username, fresh=True, )
+    refresh_token = create_refresh_token(identity=user.username)
+
+    return {'access_token': access_token, 'refresh_token': refresh_token}, 200
+
+
+@users.route('/auth/', methods=['PUT'])
+@jwt_refresh_token_required
+def get_new_access_token():
+    identity = get_jwt_identity()
+    return {'access_token': create_access_token(identity=identity)}
+
+
+@users.route('/', methods=['GET'])
+@jwt_required
+def get_user():
+    identity = get_jwt_identity()
+    user = User.query.filter(User.username.ilike(identity)).first()
+    return {'username': user.username}
